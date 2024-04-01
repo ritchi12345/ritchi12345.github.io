92722e805a1ef41363ead7de2e2b2e6deae84189
Dorghaam Haidar <dorghaam.haidar@dcmail.ca>
2024-04-01 14:28:05 -0400

    Wired up dark and light button

--- assignment4/part2/gallery-start/main.js
+++ assignment4/part2/gallery-start/main.js
@@ -27,10 +27,22 @@ for (const image of images) {
       displayedImage.alt = e.target.alt;
     });
   }
-  
+
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
 newImage.setAttribute('alt', xxx);
 thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', () => {
+  const btnClass = btn.getAttribute('class');
+  if (btnClass === 'dark') {
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+});
